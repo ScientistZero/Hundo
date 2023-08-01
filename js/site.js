3e6264f37f6db67fce9da80cad6b246ca157d79a
ScientistZero <chrisherzig@msn.com>
2023-08-01 15:11:32 -0400

    Added functions to get values

--- js/site.js
+++ js/site.js
@@ -5,16 +5,25 @@
 //start or controller function
 function getValues(){
     //get values from page
-let startValue 
-    //call generateNumbers
+let startValue = document.getElementById("startValue").value;
+let endValue = document.getElementById("endValue").value;
 
+    //call generateNumbers
+let numbers = generateNumbers(startValue,endValue);
     //call displayNumbers
 }
 
 //generate numbers from the startValue to the endValue
 //logic functions
-function generateNumbers(){
+function generateNumbers(sValue, eValue){
+    let numbers = [];
 
+    //we want to get all numbers from start to end
+for (let index = sValue; index <= eValue; index++) {
+    //this will execute in a loop until index = eValue
+    numbers.push(index);
+}
+    return numbers;
 }
 
 //display the numbers and mark the even numbers bold
