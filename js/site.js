b603983931124184fc0c58207936946e1cd131e6
ScientistZero <chrisherzig@msn.com>
2023-08-01 12:13:22 -0400

    Starting JS

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function HelloWorld(){
-    alert(hello world);
+
+
+
+//get the values from the page
+//start or controller function
+function getValues(){
+    //get values from page
+let startValue 
+    //call generateNumbers
+
+    //call displayNumbers
+}
+
+//generate numbers from the startValue to the endValue
+//logic functions
+function generateNumbers(){
+
+}
+
+//display the numbers and mark the even numbers bold
+//display or view functions
+function displayNumbers(){
+
 }
