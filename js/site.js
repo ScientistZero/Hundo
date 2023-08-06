0ee0b1b1347b5d049f207a13de3ac8d13f8a1897
ScientistZero <chrisherzig@msn.com>
2023-08-06 17:35:52 -0400

    Added ability to parse integers

--- js/site.js
+++ js/site.js
@@ -3,14 +3,28 @@
 
 //get the values from the page
 //start or controller function
-function getValues(){
+function getValues() {
     //get values from page
 let startValue = document.getElementById("startValue").value;
 let endValue = document.getElementById("endValue").value;
+let numbers = [];
+//We need to validate our input
+//parse into integers
+startValue = parseInt(startValue);
+endValue = parseInt(endValue);
+
+if (Number.isInteger(startValue) && Number.isInteger(endValue)){
 
     //call generateNumbers
-let numbers = generateNumbers(startValue,endValue);
+    let numbers = generateNumbers(startValue,endValue);
+    }else{
+alert("You must enter intgers");
+
+    }
+
+
     //call displayNumbers
+    displayNumbers(numbers);
 }
 
 //generate numbers from the startValue to the endValue
@@ -28,6 +42,12 @@ for (let index = sValue; index <= eValue; index++) {
 
 //display the numbers and mark the even numbers bold
 //display or view functions
-function displayNumbers(){
+function displayNumbers(numbers){
+let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        templateRows += `<tr><td>${number}</td></tr>`;
+    }
+document.getElementById('results').innerHTML = templateRows
 
 }
